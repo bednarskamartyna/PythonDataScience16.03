d060da50cdb313dd3eaba64457ae7e62d05e604a
bednarskamartyna <martyna.krol94@gmail.com>
2022-03-16 20:33:23 +0100

    16.03

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,33 @@
 print("Hello world")
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+jan = ("Jan", "Kowalski", 33)
+janina = ("Janina", "Nowak", (21,12,1978), 'K')
+
+imie = jan[0]
+nazwisko = jan[1]
+wiek = jan[2]
+
+imie, nazwisko, wiek = jan
+
+lista = [1,2,3,4,-5,6,-10]
+
+print(lista)
+
+print(type(lista))
+
+liczby = [0.1, 0.2, 0.3, 4.5, -7.3, 6.87, 10]
+print(type(liczby))
+
+
+imiona = ['Ala', 'Zygmunt', "Alojzy", "Bogusława"]
+
+print(imiona)
+
+lista = [1, "Berta", 3, 4, -5, "kot", -10.75, 3.14]
+print(lista[1:6])
+
+print(lista[2:6:2])
+
+print(lista[:4])
